e2deb868e6b1649fac7c2ab46a896c6c67f69e9e
Alex Maluytin <alex.malyutin.dev@gmail.com>
2025-08-16 15:28:24 +0400

    Update BlurColor pass.

--- Packages/com.alexmalyutindev.radiance-cascades-urp/Core/BlurredColorBuffer/BlurredColorBufferPass.cs
+++ Packages/com.alexmalyutindev.radiance-cascades-urp/Core/BlurredColorBuffer/BlurredColorBufferPass.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
 
 namespace AlexMalyutinDev.RadianceCascades.BlurredColorBuffer
 {
+    public class BlurredColorData : ContextItem
+    {
+        public TextureHandle BlurredColor;
+
+        public override void Reset()
+        {
+            BlurredColor = TextureHandle.nullHandle;
+        }
+    }
+
     public class BlurredColorBufferPass : ScriptableRenderPass
     {
-        private static readonly int InputMipLevel = Shader.PropertyToID("_InputMipLevel");
-        private static readonly int InputResolution = Shader.PropertyToID("_InputResolution");
+        private static readonly int InputMipLevelId = Shader.PropertyToID("_InputMipLevel");
+        private static readonly int InputResolutionId = Shader.PropertyToID("_InputResolution");
+        private static readonly int OffsetDirectionId = Shader.PropertyToID("_OffsetDirection");
 
         private readonly Material _material;
         private readonly RadianceCascadesRenderingData _radianceCascadesRenderingData;
@@ -25,94 +35,92 @@ namespace AlexMalyutinDev.RadianceCascades.BlurredColorBuffer
             _radianceCascadesRenderingData = radianceCascadesRenderingData;
         }
 
-        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        private class PassData
         {
-            // TODO: !!!
+            public TextureHandle FrameColor;
+            public TextureHandle BlurredColorBuffer;
+            public TextureHandle TempBuffer;
+            public Material Material;
+
+            public Vector4 InputResolution;
+            public Vector4 TargetResolution;
+            public int TargetMipsCount;
         }
 
-        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
         {
-            // TODO: Use resolution from settings.
-            var desc = new RenderTextureDescriptor(
-                2 * _radianceCascadesRenderingData.Cascade0Size.x,
-                2 * _radianceCascadesRenderingData.Cascade0Size.y
-            )
+            var cameraData = frameData.Get<UniversalCameraData>();
+            var resourceData = frameData.Get<UniversalResourceData>();
+            var blurredColorData = frameData.Create<BlurredColorData>();
+
+            var frameDesc = cameraData.cameraTargetDescriptor;
+
+            using var builder = renderGraph.AddUnsafePass<PassData>(nameof(BlurredColorBufferPass), out var passData);
+            builder.AllowPassCulling(false);
+
+            passData.Material = _material;
+
+            passData.InputResolution = new Vector4(frameDesc.width, frameDesc.height);
+            passData.FrameColor = resourceData.activeColorTexture;
+            builder.UseTexture(passData.FrameColor);
+
+            var targetWidth = 2 * _radianceCascadesRenderingData.Cascade0Size.x;
+            var targetHeight = 2 * _radianceCascadesRenderingData.Cascade0Size.y;
+            passData.TargetResolution = new Vector4(targetWidth, targetHeight);
+            passData.TargetMipsCount = (int)Mathf.Log(targetHeight, 2);
+
+            var desc = new TextureDesc(targetWidth, targetHeight)
             {
-                colorFormat = cameraTextureDescriptor.colorFormat,
-                sRGB = cameraTextureDescriptor.sRGB,
-                depthStencilFormat = GraphicsFormat.None,
+                name = "BlurredColorBuffer",
+                format = frameDesc.graphicsFormat,
+                wrapMode = TextureWrapMode.Clamp,
+                filterMode = FilterMode.Bilinear,
                 useMipMap = true,
                 autoGenerateMips = false,
             };
-            RenderingUtils.ReAllocateIfNeeded(
-                ref _radianceCascadesRenderingData.BlurredColorBuffer,
-                desc,
-                FilterMode.Bilinear,
-                TextureWrapMode.Clamp,
-                name: "BlurredColorBuffer"
-            );
+            passData.BlurredColorBuffer = renderGraph.CreateTexture(desc);
+            builder.UseTexture(passData.BlurredColorBuffer, AccessFlags.ReadWrite);
+            blurredColorData.BlurredColor = passData.BlurredColorBuffer;
 
+            desc.name = "Temp";
             desc.width >>= 1;
             desc.height >>= 1;
             desc.useMipMap = false;
-            RenderingUtils.ReAllocateIfNeeded(
-                ref _tempBlurBuffer,
-                desc,
-                FilterMode.Bilinear,
-                TextureWrapMode.Clamp,
-                name: "BlurredColorBuffer2"
-            );
-        }
+            passData.TempBuffer = builder.CreateTransientTexture(desc);
 
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-            if (_material == null) return;
-
-            var cmd = CommandBufferPool.Get();
-            using (new ProfilingScope(cmd, profilingSampler))
+            builder.SetRenderFunc<PassData>(static (data, context) =>
             {
-                context.ExecuteCommandBuffer(cmd);
-                cmd.Clear();
+                var cmd = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
 
-                var colorBuffer = renderingData.cameraData.renderer.cameraColorTargetHandle;
-                int width = colorBuffer.rt.width;
-                int height = colorBuffer.rt.height;
-                var mipsCount = _radianceCascadesRenderingData.BlurredColorBuffer.rt.mipmapCount;
+                cmd.SetGlobalInteger(InputMipLevelId, 0);
+                cmd.SetGlobalVector(InputResolutionId, data.InputResolution);
+                cmd.SetRenderTarget(data.BlurredColorBuffer, 0, CubemapFace.Unknown);
+                BlitUtils.BlitTexture(cmd, data.FrameColor, data.Material, 0);
 
-                cmd.SetRenderTarget(_radianceCascadesRenderingData.BlurredColorBuffer, 0, CubemapFace.Unknown);
-                cmd.SetGlobalInteger(InputMipLevel, 0);
-                cmd.SetGlobalVector(InputResolution, new Vector4(width, height));
-                BlitUtils.BlitTexture(cmd, colorBuffer, _material, 0);
+                var width = (int)data.TargetResolution.x;
+                var height = (int)data.TargetResolution.y;
 
-                
-                width = _radianceCascadesRenderingData.BlurredColorBuffer.rt.width;
-                height = _radianceCascadesRenderingData.BlurredColorBuffer.rt.height;
-                
                 // NOTE: Can't render into MipLevel+1 and read from MipLevel of the same image on DX!
                 // Work around is to render blur in two taps:
-                // horizontal blur into _tempBlurBuffer, then vertical blur into BlurredColorBuffer
-                for (int mipLevel = 1; mipLevel < mipsCount; mipLevel++)
+                // horizontal blur into TempBuffer, then vertical blur into BlurredColorBuffer
+                for (int mipLevel = 1; mipLevel < data.TargetMipsCount; mipLevel++)
                 {
-                    cmd.SetGlobalVector(InputResolution, new Vector4(width, height));
+                    cmd.SetGlobalVector(InputResolutionId, new Vector4(width, height));
 
-                    cmd.SetRenderTarget(_tempBlurBuffer, mipLevel - 1);
-                    cmd.SetGlobalInteger(InputMipLevel, mipLevel - 1);
-                    cmd.SetGlobalVector("_OffsetDirection", new Vector4(1, 0));
-                    BlitUtils.BlitTexture(cmd, _radianceCascadesRenderingData.BlurredColorBuffer, _material, 3);
+                    cmd.SetRenderTarget(data.TempBuffer, mipLevel - 1);
+                    cmd.SetGlobalInteger(InputMipLevelId, mipLevel - 1);
+                    cmd.SetGlobalVector(OffsetDirectionId, new Vector4(1, 0));
+                    BlitUtils.BlitTexture(cmd, data.BlurredColorBuffer, data.Material, 3);
 
-                    cmd.SetRenderTarget(_radianceCascadesRenderingData.BlurredColorBuffer, mipLevel);
-                    cmd.SetGlobalInteger(InputMipLevel, mipLevel - 1);
-                    cmd.SetGlobalVector("_OffsetDirection", new Vector4(0, 1));
-                    BlitUtils.BlitTexture(cmd, _tempBlurBuffer, _material, 3);
+                    cmd.SetRenderTarget(data.BlurredColorBuffer, mipLevel);
+                    cmd.SetGlobalInteger(InputMipLevelId, mipLevel - 1);
+                    cmd.SetGlobalVector(OffsetDirectionId, new Vector4(0, 1));
+                    BlitUtils.BlitTexture(cmd, data.TempBuffer, data.Material, 3);
 
                     width >>= 1;
                     height >>= 1;
                 }
-            }
-
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
-            CommandBufferPool.Release(cmd);
+            });
         }
     }
 }
