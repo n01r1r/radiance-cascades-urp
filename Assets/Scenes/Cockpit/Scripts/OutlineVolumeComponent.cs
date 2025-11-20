f67af37433a55852d3a09b2f86d824c9e547fd2e
n01r1r <handy724@ajou.ac.kr>
2025-11-20 16:00:27 +0900

    imported Blacksmith Forge asset for complex scene testing

--- Assets/Scenes/Cockpit/Scripts/OutlineVolumeComponent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.Rendering;
-
-[VolumeComponentMenu("Sample Scene/Outline")]
-public class OutlineVolumeComponent : VolumeComponent
-{
-    public BoolParameter Enabled = new BoolParameter(false);
-}
